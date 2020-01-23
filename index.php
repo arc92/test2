04b2f571ef6727679db2b9ed155b4190c8c6d937
nimak <nimaamiri92@gmail.com>
2020-01-24 01:23:21 +0330

    increace memory limit

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
+set_time_limit(0);
+ini_set('memory_limit', '20000M');
 
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/vendor/yiisoft/yii2/Yii.php';
