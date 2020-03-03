5c0a0dc4d8761fa51b6408df0acc135c6af6c873
nimak <nimaamiri92@gmail.com>
2020-03-03 18:38:38 +0330

    remove crisp chat

--- views/layouts/layout.php
+++ views/layouts/layout.php
@@ -795,7 +795,7 @@ $str.=($urls['path']);
     // });
     
 </script>
-<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="642709d0-bdbb-4c92-be13-61d8b725d594";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+<!--<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="642709d0-bdbb-4c92-be13-61d8b725d594";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>-->
 
 <?php $this->endBody() ?>
 </body>
