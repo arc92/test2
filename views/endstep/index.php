6f6b0dd0443d5451d12cb37458f348f4a486ab75
nima <nimaamiri92@gmail.com>
2020-03-14 12:36:43 +0330

    disable paradakht

--- views/endstep/index.php
+++ views/endstep/index.php
@@ -470,7 +470,9 @@ use hoomanMirghasemi\jdf\Jdf;
                         </div>
                       <!-- Html::a('پرداخت در محل ', ['name'=>'payplace','class' => 'btn payment']) ?> -->
                         <!-- <?= Html::submitButton('پرداخت در محل', ['name'=>'payplace','value'=>1,'class' => 'btn payment']) ?> -->
-<!--                        --><?//= Html::submitButton('ثبت و پرداخت', ['class' => 'btn payment']) ?>
+                        <?php
+//                        Html::submitButton('ثبت و پرداخت', ['class' => 'btn payment'])
+                        ?>
                         <!-- Html::submitButton('پرداخت در محل  ', ['name'=>'payplace','class' => 'btn payment']) ?> -->
                   
                         <!-- Html::a('پرداخت در محل   ', ['href'=>'/site/notificationpay','name'=>'payplace','class' => 'btn payment']) ?> -->
