4c493422bc48db88a3de231b78f8b423ad9bbaa8
nima amiri <nimaamiri92@gmail.com>
2020-06-02 00:00:35 +0430

    test

--- controllers/EndstepController.php
+++ controllers/EndstepController.php
@@ -294,8 +294,8 @@ class EndstepController extends Controller
 
     public function actionBackmellat($id)
     {
-        var_dump(\yii::$app->request->post());die();
         if (\yii::$app->mellatbank->Callback(\yii::$app->request->post())) {
+            var_dump(\yii::$app->request->post());die();
             $model = \app\models\Bascket::find()->where(['id' => $id])->one();
 
             // $model->refID=\yii::$app->session['ref_code'];
