67cfd7f9d5d3f5591a572302a54529926072e141
nima amiri <nimaamiri92@gmail.com>
2020-06-11 06:39:02 +0430

    some change

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -450,6 +450,8 @@ class ProductController extends Controller
         $products->where(['product.catproductID' => $catproducts->id]);
         $products->groupBy(['product.id', 'product.name', 'product.catproductID', 'product.status', 'product.catID', 'product.subcatID', 'product.planID', 'product.colorID', 'product.storePrice', 'product.price', 'product.count', 'product.description', 'product.likes', 'product.submitDate', 'product.titlemeta', 'product.descriptionmeta', 'product.off']);
         $products->orderBy(['product_Count' => SORT_DESC]);
+        $products->orderBy(['count' => SORT_DESC]);
+
 
         $category = new \app\models\Category();
         $subcat = new \app\models\Subcat();
