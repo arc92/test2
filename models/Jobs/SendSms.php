8ae1e1455ebe66b4e9575e3fe69096862fd01fda
nima-mac <n.amiri@irantalent.com>
2020-08-14 03:03:10 +0430

    sms queue

--- models/Jobs/SendSms.php
+++ models/Jobs/SendSms.php
@@ -20,19 +20,19 @@ class SendSms extends BaseObject implements JobInterface
         $smsLog->created_at = Carbon::now(\Yii::$app->timezone);
 
 
-        $response = (new Client)->request('POST', 'http://api.smsapp.ir/v2/sms/send/simple', [
-            'headers' => [
-                'apikey' => '9bQPFjT8P/UB3mhGOJGYO0/aASU/STCCZ1lk+ECNvq0'
-            ],
-            'json' => [
-                'message' => $this->message,
-                'sender' => '30005066962957',
-                'Receptor' => '09363223600',
-            ]
-        ]);
+//        $response = (new Client)->request('POST', 'http://api.smsapp.ir/v2/sms/send/simple', [
+//            'headers' => [
+//                'apikey' => '9bQPFjT8P/UB3mhGOJGYO0/aASU/STCCZ1lk+ECNvq0'
+//            ],
+//            'json' => [
+//                'message' => $this->message,
+//                'sender' => '30005066962957',
+//                'Receptor' => '09363223600',
+//            ]
+//        ]);
 
 
-        $smsLog->state = (string) (json_decode($response->getBody()->getContents())->result);
+        $smsLog->state = (string) 123;
         $smsLog->save();
     }
 }
