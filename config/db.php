fb73af1e17f9f7ffc3e83b7aca738cffc9dc8e7c
nima-mac <n.amiri@irantalent.com>
2020-08-14 07:07:14 +0430

    sms

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=' . env('MYSQL_HOST') . ';' . 'dbname=' . env('MYSQL_DATABASE'),
+    'dsn' => 'mysql:host=' . env('MYSQL_HOST','localhost') . ';' . 'dbname=' . env('MYSQL_DATABASE','bccstyle'),
     'username' => env('MYSQL_USERNAME','pro'),
     'password' => env('MYSQL_PASSWORD','Arcsin_360'),
     'charset' => 'utf8',
