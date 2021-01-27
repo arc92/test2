36668bad9c05e683ba3e2e5f4a3222cccbb5f004
server <server@server.com>
2021-01-27 23:59:30 +0000

    server chnages

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=' . env('MYSQL_HOST','localhost') . ';' . 'dbname=' . env('MYSQL_DATABASE','bccstyle'),
-    'username' => env('MYSQL_USERNAME','pro'),
+    'username' => env('MYSQL_USERNAME','root'),
     'password' => env('MYSQL_PASSWORD','Arcsin_360'),
     'charset' => 'utf8',
 
