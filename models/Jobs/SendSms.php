c51397098e03bbb796f1897797db408a8f72fb57
Nima Amiri <you@example.com>
2021-02-03 16:17:13 +0330

    fix

--- models/Jobs/SendSms.php
+++ models/Jobs/SendSms.php
@@ -15,25 +15,25 @@ class SendSms extends BaseObject implements JobInterface
     public function execute($queue)
     {
         echo 123;
-//        $smsLog = (new SmsLog);
-//        $smsLog->phone_number = (string) $this->number;
-//        $smsLog->message =  (string)$this->message;
-//        $smsLog->created_at = Carbon::now(\Yii::$app->timezone);
-//
-//
-//        $response = (new Client)->request('POST', 'http://api.smsapp.ir/v2/sms/send/simple', [
-//            'headers' => [
-//                'apikey' => '9bQPFjT8P/UB3mhGOJGYO0/aASU/STCCZ1lk+ECNvq0'
-//            ],
-//            'json' => [
-//                'message' => $this->message,
-//                'sender' => '30005066962957',
-//                'Receptor' => $this->number,
-//            ]
-//        ]);
-//
-//
-//        $smsLog->state = (string) (json_decode($response->getBody()->getContents())->result);
+        $smsLog = (new SmsLog);
+        $smsLog->phone_number = (string) $this->number;
+        $smsLog->message =  (string)$this->message;
+        $smsLog->created_at = Carbon::now(\Yii::$app->timezone);
+
+
+        $response = (new Client)->request('POST', 'http://api.smsapp.ir/v2/sms/send/simple', [
+            'headers' => [
+                'apikey' => '9bQPFjT8P/UB3mhGOJGYO0/aASU/STCCZ1lk+ECNvq0'
+            ],
+            'json' => [
+                'message' => $this->message,
+                'sender' => '30005066962957',
+                'Receptor' => $this->number,
+            ]
+        ]);
+
+
+        $smsLog->state = (string) (json_decode($response->getBody()->getContents())->result);
 //        $smsLog->save();
     }
 }
