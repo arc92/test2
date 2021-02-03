7018794f3eaf2cd9dba9f84b1865ab5583b9a3b2
Nima Amiri <you@example.com>
2021-02-03 16:33:29 +0330

    fix

--- models/Jobs/SendSms.php
+++ models/Jobs/SendSms.php
@@ -16,9 +16,9 @@ class SendSms extends BaseObject implements JobInterface
     {
         try{
             $smsLog = (new SmsLog);
-//            $smsLog->phone_number = (string) $this->number;
-//            $smsLog->message =  (string)$this->message;
-//            $smsLog->created_at = Carbon::now(\Yii::$app->timezone);
+            $smsLog->phone_number = (string) $this->number;
+            $smsLog->message =  (string)$this->message;
+            $smsLog->created_at = Carbon::now(\Yii::$app->timezone);
 
 
             $response = (new Client)->request('POST', 'http://api.smsapp.ir/v2/sms/send/simple', [
@@ -34,7 +34,7 @@ class SendSms extends BaseObject implements JobInterface
 
 
             $smsLog->state = (string) (json_decode($response->getBody()->getContents())->result);
-//            $smsLog->save();
+            $smsLog->save();
         }catch (\Exception $e){
             dd($e);
         }
