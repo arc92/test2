8a0888c3ee5c8db8de11c0625cf353c85f38bd1f
nima amiri <nimaamiri92@gmail.com>
2021-02-22 20:55:41 +0330

    Update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -321,8 +321,8 @@ $str .= ($urls['path']);
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="/branches/" class="nav-link" id="navbarDropdown" >
-                                فروشگاه ها
+                            <a href="/collections/Kids/" class="nav-link" id="navbarDropdown" >
+                                 کودک ۴تا۷ سال
                             </a>
                         </li>
 
